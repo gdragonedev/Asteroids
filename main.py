16d181b4c519818445825946a7a6dc3faf64bacc
gdragonedev <gdragonedev@gmail.com>
2024-09-02 13:41:01 -0400

    Ch2: L6-8: Player rotation, movement, and groups.

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     dt = 0
 
     while True:
@@ -17,8 +24,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        for obj in updateable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         # limit fps to 60
         dt = clock.tick(60)/1000
